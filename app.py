c15f57912fddb99cc8bb7ddbcff7f277983bff46
Carlos Ost <carlosost@Carloss-MacBook-Pro.local>
2021-01-17 13:08:23 -0300

    [WIP] stock broadcast msg false + signed user name

--- app.py
+++ app.py
@@ -46,34 +46,34 @@ messages = []
 @io.on('sendMessage')
 def send_message_handler(msg):
 
-    name = msg['name']
-    text = msg['message']
+    msg = msg['message']
     try:
-        command_start = text.index("/stock=")
+        command_start = msg.index("/stock=")
     except ValueError:
         if len(messages) == 50:
             messages.pop(0)
+        msg = {'name':current_user.name, 'message':msg}
         messages.append(msg)
         emit('getMessage', msg, broadcast=True)
     else:
         code_start = command_start + 7
-        if code_start == len(text):
+        if code_start == len(msg):
             # ex: "hello bot /stock="
             bot_msg = "You forgot to inform a stock code."
-        elif text[code_start] == " ":
+        elif msg[code_start] == " ":
             # ex: "hello bot /stock= " <-- blank after command
             # ex: "hello bot /stock= thank you" <-- blank after command
             bot_msg = "The command stock was incorrectly used."
         else:
             bot_msg = "Wait a moment. I am processing your stock quote request."
-            code_end = text.find(" ", code_start)
+            code_end = msg.find(" ", code_start)
             if code_end > 0:
                 # ex: "hello bot /stock=aapl.us " <-- blank after code
                 # ex: "hello bot /stock=aapl.us thank you" <-- blank after code
-                code = text[code_start:code_end]
+                code = msg[code_start:code_end]
             else:
                 # ex: "hello bot /stock=aapl.us" <-- code at the end
-                code = text[code_start:]
+                code = msg[code_start:]
 
             class AsyncStock(threading.Thread):
                 def run(self):
@@ -83,7 +83,7 @@ def send_message_handler(msg):
             async_check_stock = AsyncStock()
             async_check_stock.start()
 
-        emit('getMessage', {'name':name, 'message':bot_msg}, broadcast=True)
+        emit('getMessage', {'name':current_user.name, 'message':bot_msg}, broadcast=False)
 
 @io.on('message')
 def message_handler(msg):
